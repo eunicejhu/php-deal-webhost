5c670776eff93da69c81a9bc710dbc87f9c1aad2
ZUOQIN HU (Isabella) <eunicejhu@gmail.com>
2022-08-07 19:24:15 +0200

    feat: modify BASE_URL for deployment

--- view/common/checkIsAdmin.js
+++ view/common/checkIsAdmin.js
@@ -1,6 +1,6 @@
 let isAdmin = document.cookie.match(/is_admin=1/i);
 
-const BASE_URL = document.location.origin + "/webhost";
+const BASE_URL = document.location.origin;
 if (!isAdmin) {
   document.location.href = BASE_URL + "/index.php";
 }
--- view/common/checkLoggedIn.js
+++ view/common/checkLoggedIn.js
@@ -2,7 +2,7 @@ let loggedIn = document.cookie.match(new RegExp("login=true"));
 let logged_id = document.cookie.match(new RegExp("logged_id=([\\d])."));
 let isLoginPage = document.location.href.match(/login.php/i);
 
-const BASE_URL = document.location.origin + "/webhost";
+const BASE_URL = document.location.origin;
 
 if (loggedIn && isLoginPage) {
   document.location.href = BASE_URL + "/index.php";
